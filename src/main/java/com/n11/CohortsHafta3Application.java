f22945a6f8b7c30a6586555bc5ff89dbfc7fd37b
Alişan <alisankarababa@gmail.com>
2024-02-26 22:05:08 +0300

    Initial commit

---
+++ src/main/java/com/n11/CohortsHafta3Application.java
@@ -0,0 +1,13 @@
+package com.n11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohortsHafta3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CohortsHafta3Application.class, args);
+    }
+
+}
