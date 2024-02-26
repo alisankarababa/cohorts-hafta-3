114c1230678c85f777af2bd1129495ead8cfbd3e
Alişan <alisankarababa@gmail.com>
2024-02-26 22:19:00 +0300

    add BaseEntity

---
+++ src/main/java/com/n11/entity/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.n11.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    private LocalDateTime timeOfCreation;
+    private LocalDateTime timeOfUpdate;
+}
