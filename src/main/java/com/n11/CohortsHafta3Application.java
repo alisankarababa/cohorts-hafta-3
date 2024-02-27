a73f70be4d26682845bb5add9a7dfb438bacaac9
Alişan <alisankarababa@gmail.com>
2024-02-27 19:23:23 +0300

    log all customers

--- src/main/java/com/n11/CohortsHafta3Application.java
+++ src/main/java/com/n11/CohortsHafta3Application.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import java.time.LocalDate;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class CohortsHafta3Application {
+    private static final Logger logger = Logger.getLogger(CohortsHafta3Application.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(CohortsHafta3Application.class, args);
@@ -24,6 +26,10 @@ public class CohortsHafta3Application {
         customerService.save(new Customer("Ayşe", "Kaya", "aysekaya@email.com", LocalDate.of(1985, 3, 17)));
         customerService.save(new Customer("Mehmet", "Cemal", "mehmetcemal@email.com", LocalDate.of(1988, 7, 8)));
         customerService.save(new Customer("Ceren", "Demir", "cerendemir@email.com", LocalDate.of(1992, 12, 3)));
+
+
+        logger.info("All customers:\n");
+        customerService.findAll().forEach( customer -> logger.info("-" + customer + "\n"));
     }
 
 }
