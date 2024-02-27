62e31f9632e5647013a9e160cb06c5a653178bae
Alişan <alisankarababa@gmail.com>
2024-02-27 19:20:56 +0300

    save new customers in main

--- src/main/java/com/n11/CohortsHafta3Application.java
+++ src/main/java/com/n11/CohortsHafta3Application.java
@@ -1,13 +1,29 @@
 package com.n11;
 
+import com.n11.entity.Customer;
+import com.n11.service.ICustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class CohortsHafta3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(CohortsHafta3Application.class, args);
+
+        ApplicationContext applicationContext = SpringApplication.run(CohortsHafta3Application.class, args);
+
+        ICustomerService customerService = applicationContext.getBean(ICustomerService.class);
+
+
+        customerService.save(new Customer("Ali", "Veli", "aliveli@email.com", LocalDate.of(1994, 10, 11)));
+        customerService.save(new Customer("Ahmet", "Yılmaz", "ahmetyilmaz@email.com", LocalDate.of(1990, 5, 25)));
+        customerService.save(new Customer("Ayşe", "Kaya", "aysekaya@email.com", LocalDate.of(1985, 3, 17)));
+        customerService.save(new Customer("Mehmet", "Cemal", "mehmetcemal@email.com", LocalDate.of(1988, 7, 8)));
+        customerService.save(new Customer("Ceren", "Demir", "cerendemir@email.com", LocalDate.of(1992, 12, 3)));
     }
 
 }
